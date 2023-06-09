967562b4a4390f04b73e7f00f484c4ac7cef4b4f
user <user@LAPTOP-S5PHO2U3>
2023-06-09 13:12:44 +0900

    23-06-09

--- 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx09_연습2.java
+++ 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx09_연습2.java
@@ -1,2 +1,35 @@
 package step7_01.objectArray;
 
+class User1 {
+	
+	String id;
+	String pw;
+	
+	void printData() { 
+		System.out.println("이름 : " + id + "\t비밀번호  : " + pw);
+		
+	}
+}
+
+
+class Controller1 {
+	
+	User1[] list = null;
+	int stdCnt = 0;
+	
+	void addUser(User1 st) {
+		int 
+	}
+	void delUser(User1 st) {
+		
+	}
+	String outData() { 
+		return data;
+	}
+	String saveData() {
+		return data;
+	}
+	void printUser() {
+		
+	}
+}
